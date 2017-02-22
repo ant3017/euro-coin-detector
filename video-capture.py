701af980f08712cb95ea403f007af99313e22cd6
Chen Yumin <hello@chenyumin.com>
2017-02-22 15:12:32 +0000

    Fix frame size

--- video-capture.py
+++ video-capture.py
@@ -1,14 +1,16 @@
 import numpy as np
 import cv2
 
+FRAME_WIDTH = 640
+
 def run_main():
     cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
     while(True):
         ret, frame = cap.read()
-        roi = frame[0:500, 0:500]
+        roi = cv2.resize(frame, (FRAME_WIDTH, FRAME_WIDTH * frame.shape[0] / frame.shape[1]))
         gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
         gray_blur = cv2.GaussianBlur(gray, (15, 15), 0)
