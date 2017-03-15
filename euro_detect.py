3e7c463b6c6d563ca7aa8a36e2e7d30c32fba6b3
Chen Yumin <hello@chenyumin.com>
2017-03-15 21:14:27 +0000

    Create class Coin

--- euro_detect.py
+++ euro_detect.py
@@ -3,15 +3,41 @@ import cv2
 
 FRAME_WIDTH = 640
 
-def euro_detect(rgb_stream):
+
+
+class Coin:
+    """Coin class for a euro coin."""
+
+    def __init__(self, x, y, r):
+        """Initializes the euro coin object."""
+        self.x = x;
+        self.y = y;
+        self.r = r;
+        self.center_hue = self.__get_center_hue()
+
+    def __get_center_hue(self):
+        """Update the hue of this coin's center area."""
+        # Only use half of the coin area to determine it's center color
+        r = self.r * 0.5
+
+        roi = Coin.hsv[int(self.y-r):int(self.y+r), int(self.x-r):int(self.x+r)]
+
+        if len(roi) > 0:
+            return (sum([pixel[0] for rows in roi for pixel in rows])
+                / len(roi) / len(roi[0]))
+
+
+def euro_detect(rgb):
     """Detect the euro coins from a RGB stream of a colored image and returns
     the detection results."""
 
-    gray = cv2.cvtColor(rgb_stream, cv2.COLOR_RGB2GRAY)
-    hsv = cv2.cvtColor(rgb_stream, cv2.COLOR_RGB2HSV)
+    # Assign Coin class variables
+    Coin.rgb = rgb
+    Coin.gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
+    Coin.hsv = cv2.cvtColor(rgb, cv2.COLOR_RGB2HSV)
 
     # Adaptive Thresholding
-    gray_blur = cv2.GaussianBlur(gray, (15, 15), 0)
+    gray_blur = cv2.GaussianBlur(Coin.gray, (15, 15), 0)
     thresh = cv2.adaptiveThreshold(gray_blur, 255,
         cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
 
@@ -20,58 +46,46 @@ def euro_detect(rgb_stream):
                                 param1=20, param2=40, minRadius=24,
                                 maxRadius=96)
 
-
-
-    # Color segmentation
+    coins = None
     if circles is not None:
-        circles = [circle[0] for circle in circles.tolist()]
-        for i in range(len(circles)):
-            c = circles[i]
-            x, y, r = c[0], c[1], c[2]
+        # Initialize coins using those detected circles
+        coins = [Coin(circle[0][0], circle[0][1], circle[0][2])
+            for circle in circles.tolist()]
 
-            # Only use half of the coin area to determine it's center color
-            r = r * 0.5
-
-            roi = hsv[int(y-r):int(y+r), int(x-r):int(x+r)]
-            #print roi
-            hue_avg = sum([pixel[0] for rows in roi for pixel in rows]) / len(roi) / len(roi[0])
-
-            circles[i].append(hue_avg)
-            print circles[i]
 
     # cv2.imwrite('gray_blur.jpg', gray_blur)
     # cv2.imwrite('thresh.jpg', thresh)
 
-    return circles
+    return coins
 
 
 def demo(roi):
-    circles = euro_detect(roi)
+    coins = euro_detect(roi)
 
     height, width, depth = roi.shape
 
     circle_mask = np.zeros((height, width), np.uint8)
 
-    if circles is not None:
+    if coins is not None:
 
-        for c in circles:
+        for c in coins:
             # Draw into the circle mask
-            cv2.circle(circle_mask, (int(c[0]), int(c[1])), int(c[2]),
+            cv2.circle(circle_mask, (int(c.x), int(c.y)), int(c.r),
                 1, thickness=-1)
     masked_data = cv2.bitwise_and(roi, roi, mask=circle_mask)
 
-    if circles is not None:
-        for c in circles:
-            x, y, r = c[0], c[1], c[2]
+    if coins is not None:
+        for c in coins:
             # Draw the outer circle
-            cv2.circle(roi, (int(x), int(y)), int(r),
+            cv2.circle(roi, (int(c.x), int(c.y)), int(c.r),
                 (0, 255, 0), 2)
             # Draw the center of the circle
-            cv2.circle(roi, (int(x), int(y)), 2, (0, 0, 255), 3)
+            cv2.circle(roi, (int(c.x), int(c.y)), 2, (0, 0, 255), 3)
             # Draw the hue sampling area
-            cv2.rectangle(roi, (int(x - r*0.5), int(y - r*0.5)), (int(x + r*0.5), int(y + r*0.5)), (0, 255, 0), 1)
+            cv2.rectangle(roi, (int(c.x - c.r*0.5), int(c.y - c.r*0.5)),
+                (int(c.x + c.r*0.5), int(c.y + c.r*0.5)), (0, 255, 0), 1)
             # Draw the descriptive text
-            cv2.putText(roi, "Hue " + str(c[3]), (int(x), int(y)),
+            cv2.putText(roi, "Hue " + str(c.center_hue), (int(c.x), int(c.y)),
                 cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255), 2)
 
 
